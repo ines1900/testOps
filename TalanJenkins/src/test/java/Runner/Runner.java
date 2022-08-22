332f5b3cc3c97f3db4a409bb31d45b6409ac12c9
imasmoudi <imasmoudi@TALAN-7DHP56L.dom.tti>
2022-08-22 14:47:40 +0100

    creation

---
+++ TalanJenkins/src/test/java/Runner/Runner.java
@@ -0,0 +1,18 @@
+package Runner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="src/test/resources/Features/TalanJenkins.feature", 
+		glue= {"StepsDef"},
+		strict=true,                   
+		monochrome=true,
+		plugin = {"pretty","json:target/JSONReports/report.json","html:target/HtmlReports"}
+		)
+public class Runner {
+
+}
+
